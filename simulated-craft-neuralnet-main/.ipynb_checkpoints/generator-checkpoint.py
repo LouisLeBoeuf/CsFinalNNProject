99538e2494d15e1fd9dcf92f64ad83c3fcfb6c8a
LouisLeBoeuf <clleboeuf@crimson.ua.edu>
2023-12-14 13:45:20 -0600

    Added SimulatedCraftProject

---
+++ simulated-craft-neuralnet-main/.ipynb_checkpoints/generator-checkpoint.py
@@ -0,0 +1,51 @@
+import geneticalgorithms as ga
+import realtimeviewer as rtv
+from shipsims import drone
+from shipsims import gimbaldrone
+from shipsims import ship
+from shipsims import spinship
+import numpy as np
+
+######################################################
+#You can change the drone type to 
+#sim_class = ship.Ship
+#sim_class = spinship.SpinShip
+#sim_class = drone.Drone
+#sim_class = gimbaldrone.GimbalDrone
+#make sure to change the layer sizes to specific drone input and output
+#You can change the generation_limit to howver many generations to train
+#You can also change pop mode "generate" to "read" to continue previous simulation or generate a fresh population
+######################################################
+
+
+np.random.seed(42);
+
+# population, generation = run_evolution(
+#     populate_func=partial(generate_population, size=10, layer_sizes=layer_sizes),
+#     fitness_limit = 50000,
+#     generation_limit = 200
+# )
+
+
+#ship can thrust in any direction without worry of angles Must have 4 inputs and 2 outputs [4,4,2]
+#spinship is one thruster ship  with angle control must have 5 inputs and 2 outputs [5,4,2]
+#drone is a 2 thruster ship they can thrust up and down extremely unstable must have 5 inputs and 2 outputs [5,4,2]
+#gimbaldrone is a 2 gimballed thruster ship must have 5 inputs and 4 outputs [5,4,4]
+
+ls = [5,4,4] #size of the neural network to use
+sim_class = gimbaldrone.GimbalDrone #define which drone type to use spinship.
+ps = 1/30.0
+
+population, generation = ga.run_evolution(
+    # populate_func=partial(read_population),
+    sim_class = sim_class,
+    pop_mode = "generate", # Generate new genomes or read from previous trial "generate" or "read"
+    #pop_file = "savedgenome.txt",
+    max_time = 15, #max time to allow per generation
+    fitness_limit = 50000, #max fitness one goal is worth 10,000
+    generation_limit = 200, #how many generations to run
+    layer_sizes = ls 
+)
+
+view = rtv.RealtimeViewer(population[0], ls, sim_class, ps)
+view.start()
---
+++ simulated-craft-neuralnet-main/README.md
@@ -0,0 +1,32 @@
+# simulated-craft-neuralnet
+Controlling simulated spacecraft with genetic algorithm &amp; neural network
+
+This project was created in collaboration with Louis Leboeuf.
+
+If you wish to run this project, you will need:
+- pygame
+
+NOTE: This is still a work in progress.
+The user-friendly interface is not complete. Run generator.py and edit parameters within to get an idea of how this project works (sorry).
+
+# What is this project?
+This project provides:
+- Multiple simlulated craft, each with their own unique control schemes (two bidirectional thrusters, single gimbaled thrust, dual gimbaled thrusters, etc.)
+- Neural network and genetic algorithm training modules
+- Ability to train the neural networks to fly these craft to their 'goal destinations'
+- A realtime visualization module to view both the craft and the activity of the neural network
+
+# Example
+Here's the results after 500 generations of evolution:
+
+https://user-images.githubusercontent.com/69018340/120042115-18caa000-bfcf-11eb-86e0-71c8069b70dd.mp4
+
+As you can see, there is still some room for improvement, especially with this complex dual-thruster configuration.
+
+This project was inspired by John Buffer's AutoDrone project, available [here](https://github.com/johnBuffer/AutoDrone)
+
+Other resources used:
+
+[Sebastian Lague: Neural Networks series](https://www.youtube.com/watch?v=bVQUSndDllU)
+
+[Kie Codes: Genetic Algorithm in Python](https://www.youtube.com/watch?v=nhT56blfRpE)
